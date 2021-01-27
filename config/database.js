bc3a61f94ccfa3fa01dee145f766517ddc7eb8cd
njm222 <nathan.john.menezes@gmail.com>
2021-01-27 12:40:24 +0530

    mongo config update

--- config/database.js
+++ config/database.js
@@ -4,10 +4,21 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        uri: process.env.MONGO_URI
+        srv: true,
+        uri: env('MONGO_URI'),
+        ssl: { rejectUnauthorized: false }
       },
       options: {
-        ssl: true
+        ssl: true,
+        authenticationDatabase: '',
+        useUnifiedTopology: true,
+        pool: {
+          min: 0,
+          max: 10,
+          idleTimeoutMillis: 30000,
+          createTimeoutMillis: 30000,
+          acquireTimeoutMillis: 30000
+        }
       },
     },
   },
